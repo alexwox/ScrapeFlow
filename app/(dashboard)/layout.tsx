4cbe712662fd1d629886a7714ebf1750dad36121
alexwox <alexander.woxstrom@gmail.com>
2025-01-11 23:55:04 +0100

    header

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Separator } from '@/components/ui/separator';
+
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+    return <div className="flex h-screen">
+        <div className="flex flex-col flex-1 min-h-screen">
+            <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
+                ScrapeFlow
+            </header>
+            <Separator />
+            <div className="overflow-auto">
+                <div className="flex-1 container-py text-accent-foreground">
+                    {children}
+                </div>
+            </div>
+        </div>
+    </div>;
+}
+
+export default DashboardLayout;
