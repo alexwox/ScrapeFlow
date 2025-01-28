8a15107d150902b23d15b8e42c14b4b740bab2d8
alexwox <alexander.woxstrom@gmail.com>
2025-01-28 21:56:27 +0100

    link to editor

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -5,9 +5,14 @@ import { WorkflowStatus } from "@/types/workflow";
 import { FileTextIcon } from "lucide-react";
 import { Workflow } from "@prisma/client";
 import { PlayIcon } from "lucide-react";
-
+import Link from "next/link";
 import React from "react";
 import { cn } from "@/lib/utils";
+
+const statusColors = {
+    [WorkflowStatus.DRAFT]: "bg-yellow-400 text-yellow-600",
+    [WorkflowStatus.PUBLISHED]: "bg-primary",
+}
 
 function WorkflowCard({ workflow }: { workflow: Workflow }) {
     const isDraft = workflow.status === WorkflowStatus.DRAFT;
@@ -17,11 +22,18 @@ function WorkflowCard({ workflow }: { workflow: Workflow }) {
         dark:shadow-primary/30">
             <CardContent className="p4 flex items-center justify-between h-[100px]">
                 <div className={cn(
-                    "w-10 h-10 rounded-full flex items-center bg-red-500 justify-center"
+                    "w-10 h-10 rounded-full flex items-center justify-center",
+                    statusColors[workflow.status as WorkflowStatus]
                 )}>
                     {isDraft ? (
                         <FileTextIcon className="h-5 w-5" />
                     ) : (<PlayIcon className="h-5 w-5 text-white" />)}
+                </div>
+                <div className="">
+                    <h3 className="text-base font-bold text-muted-foreground flex items-center">
+                        <Link href={`/workflows/editor${workflow.id}`} 
+                        className="flex items-cetner hover:underline">{workflow.name}</Link>
+                    </h3>
                 </div>
             </CardContent>
         </Card>
