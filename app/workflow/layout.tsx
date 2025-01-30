735199e498643b6db85063b2637f0a6a9b1b1f1d
alexwox <alexander.woxstrom@gmail.com>
2025-01-30 23:57:34 +0100

    layout workflow

---
+++ app/workflow/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function layout({children}:{children:React.ReactNode} ) {
+  return (
+    
+    <div>layout<{children}:{children:React.ReactNode} /div>
+ 
+  )
+}
+
+export default layout{children}:{children:React.ReactNode} 
