46196eb07649464225c85a100e3f6f7a32d614f3
alexwox <alexander.woxstrom@gmail.com>
2025-01-31 00:31:19 +0100

    editor callbacks

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -4,17 +4,24 @@ import { Workflow } from '@prisma/client'
 import { ReactFlow, useEdgesState, useNodesState } from '@xyflow/react'
 import React from 'react'
 
+import "@xyflow/react/dist/style.css";
+
 function FlowEditor({ workflow }: { workflow: Workflow }) {
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+    const [nodes, setNodes, onNodesChange] = useNodesState([]);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
     return (
-    <main className="h-full w-full">
-        <ReactFlow>
+        <main className="h-full w-full">
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onEdgesChange={onEdgesChange}
+                onNodesChange={onNodesChange}
+            >
 
-        </ReactFlow>
-    </main>
-  )
+            </ReactFlow>
+        </main>
+    )
 }
 
 export default FlowEditor
