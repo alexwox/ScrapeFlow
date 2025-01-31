98b2f390001efffcb37523771e6d22ee1377355a
alexwox <alexander.woxstrom@gmail.com>
2025-01-31 21:40:06 +0100

    initial node for launchbrowser

--- actions/workflows/createWorkflow.ts
+++ actions/workflows/createWorkflow.ts
@@ -1,11 +1,15 @@
 "use server"
 
 import { prisma } from "@/lib/prisma";
+import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
 import { createWorkflowSchema } from "@/schema/workflows";
 
 import { createWorkflowSchemaType } from "@/schema/workflows";
+import { AppNode } from "@/types/appNode";
+import { TaskType } from "@/types/task";
 import { WorkflowStatus } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
+import { Edge } from "@xyflow/react";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
@@ -21,12 +25,19 @@ export async function createWorkflow(form: createWorkflowSchemaType) {
         throw new Error("unauthenticated");
     }
 
+    const initialFlow: {nodes: AppNode[], edges: Edge[]} = {
+        nodes: [],
+        edges: [],
+    }
+
+    //Add initial flow entry point
+    initialFlow.nodes.push(CreateFlowNode(TaskType.LAUNCH_BROWSER))
 
     const result = await prisma.workflow.create({
         data: {
             userId,
             status: WorkflowStatus.DRAFT,
-            definition: "{}",
+            definition: JSON.stringify(initialFlow),
             ...data,
         }
     })
