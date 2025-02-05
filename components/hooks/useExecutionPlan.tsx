7af04b9e7ffc0d7149ae6ac14156f768e810a63c
alexwox <alexander.woxstrom@gmail.com>
2025-02-05 21:37:29 +0100

    generate an execution plan

--- components/hooks/useExecutionPlan.tsx
+++ components/hooks/useExecutionPlan.tsx
@@ -1,12 +1,14 @@
 import { useReactFlow } from "@xyflow/react";
 import { useCallback } from "react";
+import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
+import { AppNode } from "@/types/appNode";
 
 const useExecutionPlan = () => {
   const { toObject } = useReactFlow();
 
   const generateExecutionPlan = useCallback(() => {
     const { nodes, edges } = toObject();
-    const result = FlowToExecutionPlan(nodes, edges);
+    const result = FlowToExecutionPlan(nodes as AppNode[], edges);
   }, [toObject]);
 
   return generateExecutionPlan;
--- lib/workflow/executionPlan.ts
+++ lib/workflow/executionPlan.ts
@@ -1,6 +1,9 @@
 import { AppNode } from "@/types/appNode";
-import { WorkflowExecutionPlan } from "@/types/workflow";
-import { Edge } from "@xyflow/react";
+import {
+  WorkflowExecutionPlan,
+  WorkflowExecutionPlanPhase,
+} from "@/types/workflow";
+import { Edge, getIncomers } from "@xyflow/react";
 import { TaskRegistry } from "./task/registry";
 
 type FlowToExecutionPlanType = {
@@ -18,12 +21,76 @@ export function FlowToExecutionPlan(
   if (!entryPoint) {
     throw new Error("TODO: Handle this error");
   }
-
+  const planned = new Set<string>();
   const executionPlan: WorkflowExecutionPlan = [
     {
       phase: 1,
       nodes: [entryPoint],
     },
   ];
+
+  for (
+    let phase = 2;
+    phase <= nodes.length || planned.size < nodes.length;
+    phase++
+  ) {
+    const nextPhase: WorkflowExecutionPlanPhase = { phase, nodes: [] };
+    for (const currentNode of nodes) {
+      if (planned.has(currentNode.id)) {
+        // Node already in plan
+        continue;
+      }
+      const invalidInputs = getInvalidInputs(currentNode, edges, planned);
+      if (invalidInputs.length > 0) {
+        const incomers = getIncomers(currentNode, nodes, edges);
+        if (incomers.every((incomer) => planned.has(incomer.id))) {
+          //If all incommers are planned and there are still invalid input"
+          console.error("invalid inputs", currentNode.id, invalidInputs);
+          throw new Error("TODO: Handle error 1");
+        } else {
+          continue;
+        }
+      }
+      nextPhase.nodes.push(currentNode);
+      planned.add(currentNode.id);
+    }
+  }
+
   return { executionPlan };
+}
+
+function getInvalidInputs(node: AppNode, edges: Edge[], planned: Set<string>) {
+  const invalidInputs = [];
+  const inputs = TaskRegistry[node.data.type].inputs;
+  for (const input of inputs) {
+    const inputValue = node.data.inputs[input.name];
+    const inputValueProvided = inputValue?.length > 0;
+    if (inputValueProvided) {
+      //this input is ok
+      continue;
+    }
+
+    const incomingEdges = edges.filter((edge) => edge.target === node.id);
+
+    const inputLinkedToOutput = incomingEdges.find(
+      (edge) => edge.targetHandle === input.name
+    );
+
+    const requiredInputProvidedByVisitedOutput =
+      input.required &&
+      inputLinkedToOutput &&
+      planned.has(inputLinkedToOutput.source);
+
+    if (requiredInputProvidedByVisitedOutput) {
+      continue;
+    } else if (!input.required) {
+      if (!inputLinkedToOutput) continue;
+      if (inputLinkedToOutput && planned.has(inputLinkedToOutput.source)) {
+        continue;
+      }
+    }
+
+    invalidInputs.push(input.name);
+  }
+  return invalidInputs;
 }
