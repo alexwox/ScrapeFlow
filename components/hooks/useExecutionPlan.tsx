6d41f478c148cf518530afb9b8730cd795265537
alexwox <alexander.woxstrom@gmail.com>
2025-02-07 20:09:51 +0100

    handle errors in execution plan

--- components/hooks/useExecutionPlan.tsx
+++ components/hooks/useExecutionPlan.tsx
@@ -1,14 +1,42 @@
 import { useReactFlow } from "@xyflow/react";
 import { useCallback } from "react";
-import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
+import {
+  FlowToExecutionPlan,
+  FlowToExecutionPlanValidationError,
+} from "@/lib/workflow/executionPlan";
 import { AppNode } from "@/types/appNode";
+import useFlowValdiation from "./useFlowValidation";
+import { toast } from "sonner";
 
 const useExecutionPlan = () => {
   const { toObject } = useReactFlow();
+  const { setInvalidInputs, clearErrors } = useFlowValdiation();
 
+  const handleError = useCallback((error: any) => {
+    switch (error.type) {
+      case FlowToExecutionPlanValidationError.NO_ENTRY_POINT:
+        toast.error("No entrypoint found");
+        break;
+      case FlowToExecutionPlanValidationError.INVALID_INPUTS:
+        toast.error("Not all input values are set");
+        setInvalidInputs(error.invalidElements);
+        break;
+      default:
+        toast.error("Something wen't wrong");
+        break;
+    }
+  }, []);
   const generateExecutionPlan = useCallback(() => {
     const { nodes, edges } = toObject();
-    const { executionPlan } = FlowToExecutionPlan(nodes as AppNode[], edges);
+    const { executionPlan, error } = FlowToExecutionPlan(
+      nodes as AppNode[],
+      edges
+    );
+
+    if (error) {
+      handleError(error);
+      return null;
+    }
     return executionPlan;
   }, [toObject]);
 
