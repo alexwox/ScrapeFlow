8f786556f594ed66c084333cd9f3d936a1842bf5
alexwox <alexander.woxstrom@gmail.com>
2025-02-09 17:02:23 +0100

    show validation errors in editor

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -1,26 +1,30 @@
+"use client";
 import { Workflow } from "@prisma/client";
 import React from "react";
 import { ReactFlowProvider } from "@xyflow/react";
 import FlowEditor from "./FlowEditor";
 import Topbar from "@/app/workflow/_components/topbar/Topbar";
 import TaskMenu from "./TaskMenu";
+import { FlowValidationContextProvider } from "@/components/context/FlowValidationContext";
 
 function Editor({ workflow }: { workflow: Workflow }) {
   return (
-    <ReactFlowProvider>
-      <div className="flex flex-col h-full w-full overflow-hidden">
-        <Topbar
-          title="Workflow editor"
-          subtitle={workflow.name}
-          workflowId={workflow.id}
-        />
+    <FlowValidationContextProvider>
+      <ReactFlowProvider>
+        <div className="flex flex-col h-full w-full overflow-hidden">
+          <Topbar
+            title="Workflow editor"
+            subtitle={workflow.name}
+            workflowId={workflow.id}
+          />
 
-        <section className="flex h-full overflow-auto">
-          <TaskMenu />
-          <FlowEditor workflow={workflow}></FlowEditor>
-        </section>
-      </div>
-    </ReactFlowProvider>
+          <section className="flex h-full overflow-auto">
+            <TaskMenu />
+            <FlowEditor workflow={workflow}></FlowEditor>
+          </section>
+        </div>
+      </ReactFlowProvider>
+    </FlowValidationContextProvider>
   );
 }
 
--- app/workflow/_components/nodes/NodeCard.tsx
+++ app/workflow/_components/nodes/NodeCard.tsx
@@ -1,44 +1,48 @@
-"use client"
+"use client";
 
+import useFlowValdiation from "@/components/hooks/useFlowValidation";
 import { cn } from "@/lib/utils";
 import { useReactFlow } from "@xyflow/react";
-import { ReactNode } from "react"
-
+import { ReactNode } from "react";
 
 function NodeCard({
-    children,
-    nodeId,
-    isSelected,
+  children,
+  nodeId,
+  isSelected,
 }: {
-    nodeId: string;
-    children: ReactNode;
-    isSelected: boolean;
+  nodeId: string;
+  children: ReactNode;
+  isSelected: boolean;
 }) {
-    const { getNode, setCenter } = useReactFlow();
-    return (
-        <div
-            onDoubleClick={() => {
-                const node = getNode(nodeId);
-                if (!node) return;
-                const { position, measured } = node;
-                if (!position || !measured) return;
-                const { width, height } = measured;
-                const x = position.x + width! / 2;
-                const y = position.y + height! / 2;
-                if (x === undefined || y === undefined) return;
-                setCenter(x, y, {
-                    zoom: 1, 
-                    duration: 500,
-                });
-            }}
-            className={cn(
-                "rounded-md cursor-pointer bg-background border-2 border-separate w-[500px] text-sx gap-1 flex flex-col",
-                isSelected && "border-primary"
-            )}
-        >
-            {children}
-        </div>
-    )
+  const { getNode, setCenter } = useReactFlow();
+  const { invalidInputs } = useFlowValdiation();
+  const hasInvalidInputs = invalidInputs.some((node) => node.nodeId === nodeId);
+
+  return (
+    <div
+      onDoubleClick={() => {
+        const node = getNode(nodeId);
+        if (!node) return;
+        const { position, measured } = node;
+        if (!position || !measured) return;
+        const { width, height } = measured;
+        const x = position.x + width! / 2;
+        const y = position.y + height! / 2;
+        if (x === undefined || y === undefined) return;
+        setCenter(x, y, {
+          zoom: 1,
+          duration: 500,
+        });
+      }}
+      className={cn(
+        "rounded-md cursor-pointer bg-background border-2 border-separate w-[500px] text-sx gap-1 flex flex-col",
+        isSelected && "border-primary",
+        hasInvalidInputs && "border-destructive border-2"
+      )}
+    >
+      {children}
+    </div>
+  );
 }
 
-export default NodeCard
+export default NodeCard;
