cd1321c97c11b44c9aecb8a1091a2138dab43094
alexwox <alexander.woxstrom@gmail.com>
2025-02-10 22:17:19 +0100

    puppeteer headless launch

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -4,7 +4,7 @@ import puppeteer from "puppeteer";
 export async function LaunchBrowserExecutor(): Promise<boolean> {
   try {
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
     });
     await waitFor(3000);
     await browser.close();
