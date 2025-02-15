e1481cd1166227af2085e72527805483345dfaae
alexwox <alexander.woxstrom@gmail.com>
2025-02-15 13:49:04 +0100

    added execution plan to db schema for workflows

--- actions/workflows/publishWorkflow.ts
+++ actions/workflows/publishWorkflow.ts
@@ -9,20 +9,23 @@ import {
   WorkflowExecutionPlan,
   WorkflowExecutionStatus,
   WorkflowExecutionTrigger,
+  WorkflowStatus,
 } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-export async function PublishWorkflow(form: {
+export async function PublishWorkflow({
+  workflowId,
+  flowDefinition,
+}: {
   workflowId: string;
-  flowDefinition?: string;
+  flowDefinition: string;
 }) {
   const { userId } = auth();
   if (!userId) {
     throw new Error("Unauthenticated");
   }
 
-  const { workflowId, flowDefinition } = form;
   if (!workflowId) {
     throw new Error("WorkflowID is required");
   }
@@ -36,5 +39,20 @@ export async function PublishWorkflow(form: {
 
   if (!workflow) {
     throw new Error("Workflow not found");
+  }
+
+  if (workflow.status !== WorkflowStatus.DRAFT) {
+    throw new Error("Workflow is not a draft");
+  }
+
+  const flow = JSON.parse(flowDefinition);
+  const result = FlowToExecutionPlan(flow.nodes, flow.edges);
+
+  if (result.error) {
+    throw new Error("Flow definition not valid");
+  }
+
+  if (!result.executionPlan) {
+    throw new Error("No nxecution plan generated");
   }
 }
