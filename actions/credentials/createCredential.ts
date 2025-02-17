3ac272f162a3b13b8aad6a6403552900fb8f788d
alexwox <alexander.woxstrom@gmail.com>
2025-02-17 15:52:55 +0100

    implement encryption and decryption

--- actions/credentials/createCredential.ts
+++ actions/credentials/createCredential.ts
@@ -1,11 +1,38 @@
 "use server";
 
+import { prisma } from "@/lib/prisma";
+import {
+  createCredentialSchema,
+  createCredentialSchemaType,
+} from "@/schema/credential";
 import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
-export async function CreateCredential() {
+export async function CreateCredential(form: createCredentialSchemaType) {
+  const { success, data } = createCredentialSchema.safeParse(form);
+  if (!success) {
+    throw new Error("Invalid form data");
+  }
+
   const { userId } = auth();
 
   if (!userId) {
     throw new Error("unauthorized");
   }
+
+  // Encrypt the value
+  const encryptedValue = symmetricEncrypt(data.value);
+  const result = prisma.credential.create({
+    data: {
+      userId,
+      name: data.name,
+      value: encryptedValue,
+    },
+  });
+
+  if (!result) {
+    throw new Error("Failed to create credential");
+  }
+
+  revalidatePath("/credentials");
 }
